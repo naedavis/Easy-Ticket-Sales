f0671074b50eaf61d55e6cb106b17f4b9a46a8ab
Naeemah Davis <82158710+naedavis@users.noreply.github.com>
2021-07-29 13:07:57 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -7,13 +7,42 @@ from tkinter import messagebox
 root = Tk()
 root.geometry("600x700")
 root.title("Ticket Sales")
-root.config(bg= "black")
+root.config(bg= "purple")
 variable=StringVar(root)
 
 class TicketSales():
     #variable going to be used as default value in  drop down menu
     variable.set("Select your choice")
     def __init__(self, window):
+<<<<<<< HEAD
+        # self.default = StringVar()
+        # self.lbltitle = Label(window, text="Ticket Booth ", font="Purisa 30", fg="white", bg="purple")
+        # self.lbltitle.place(x=170, y=50)
+        self.Display = Label(window,text="Ticket Booth", bg="purple", fg="white", font="Purisa 30")
+        self.Display.place(x=100, y=70, width=400, height=200)
+        self.lblCell = Label(window, text="Enter CellNumber: ", font="Arial 15", fg="white", bg="purple")
+        self.lblCell.place(x=40, y=360)
+        self.entry1 = Entry(window, bg="white")
+        self.entry1.place(x=350, y=360, width= 200)
+        self.lblCategory = Label(window, text="Select Ticket Category: ", font="Arial 15", fg="white", bg="purple")
+        self.lblCategory.place(x=40, y=420)
+        self.menuCat = OptionMenu(window, variable, "Soccer", "Movies", "Theater")
+        self.menuCat.place(x=350, y=420, width=200)
+
+        self.lblTicket = Label(window, text="Number of Tickets Bought", font="Arial 15", bg="purple", fg="white")
+        self.lblTicket.place(x=40, y=480)
+        self.spnbox = Spinbox(window, from_ =1, to= 500, bg="white")
+        self.spnbox.place(x=400, y=480, width= 70, height= 30)
+        self.btnCalc = Button(window, text= "Calculate Amount", font="Arial 15 ",fg="purple", bg="white", command= self.Calculate)
+        self.btnCalc.place(x=60, y=550)
+        self.btnClear = Button(window, text="Clear", font= "Arial 15",fg="purple", bg="white", command= self.Clear)
+        self.btnClear.place(x=400, y=550, width= 100)
+
+
+    def menOp(self):
+        self.default.get()
+
+=======
         #creating label on window
         self.lblCell = Label(window, text="Enter CellNumber: ", font="Arial 15", fg="white", bg="black")
         self.lblCell.place(x=40, y=60)
@@ -45,6 +74,7 @@ class TicketSales():
         self.btnClear = Button(window, text="Clear", font= "Arial 15", bg="blue", fg="white", command= self.Clear)
         self.btnClear.place(x=400, y=250, width= 100)
     #function that will calculate ticket prices and display them
+>>>>>>> origin/main
     def Calculate(self):
         #defining a variable that will take in the value selected in the spinbox by the user
         number = self.spnbox.get()
@@ -58,8 +88,12 @@ class TicketSales():
                 if len(self.entry1.get()) == 10 or num[0] == 0:
                     #calculation for soccer tickets
                     total = int(self.spnbox.get()) * 40
+<<<<<<< HEAD
+                    self.Display.config(font="Arial 20", text="Amount Payable: R" + str(total + (0.14 * total)) + "\n" +
+=======
                     #making the label created equal to the amount payable and the chosen values by the user
                     self.Display.config(text="Amount Payable: R" + str(total + (0.14 * total)) + "\n" +
+>>>>>>> origin/main
                                              "Reservation for " + f' {variable.get()} ' + " for " + number + "\n" +
                                              "was done by " + self.entry1.get())
                 else:
@@ -69,8 +103,12 @@ class TicketSales():
                     #clear the values enterd ny the user
                     self.entry1.delete(0, END)
                     self.spnbox.delete(0, END)
+<<<<<<< HEAD
+                    self.Display.config(text=" Ticket Booth ")
+=======
                     self.Display.config(text=" ")
             # if the user chooses movies
+>>>>>>> origin/main
             elif variable.get() == "Movies":
                 #value of entry to be stored in num variable 
                 num = self.entry1.get()
@@ -78,8 +116,12 @@ class TicketSales():
                 if len(self.entry1.get()) == 10 or num[0] == 0:
                     #calculation for movie tickets
                     total = int(self.spnbox.get()) * 75
+<<<<<<< HEAD
+                    self.Display.config(font="Arial 20",text="Amount Payable: R" + str(total + (0.14 * total)) + "\n" +
+=======
                      #making the label created equal to the amount payable and the chosen values by the user
                     self.Display.config(text="Amount Payable: R" + str(total + (0.14 * total)) + "\n" +
+>>>>>>> origin/main
                                              "Reservation for " + f' {variable.get()} ' + " for " + number + "\n" +
                                              "was done by " + self.entry1.get())
                 else:
@@ -97,8 +139,12 @@ class TicketSales():
                 if len(self.entry1.get()) == 10 or num[0] == 0:
                     #calculation for theater tickets
                     total = int(self.spnbox.get()) * 100
+<<<<<<< HEAD
+                    self.Display.config(font="Arial 20",text="Amount Payable: R" + str(total + (0.14 * total)) + "\n" +
+=======
                     #making the label created equal to the amount payable and the chosen values by the user
                     self.Display.config(text="Amount Payable: R" + str(total + (0.14 * total)) + "\n" +
+>>>>>>> origin/main
                                              "Reservation for " + f' {variable.get()} ' + " for " + number + "\n" +
                                              "was done by " + self.entry1.get())
                 else:
@@ -115,7 +161,8 @@ class TicketSales():
     def Clear(self):
         self.entry1.delete(0,END)
         self.spnbox.delete(0, END)
-        self.Display.config( text = " ")
+        self.Display.config( text = " Ticket Booth ")
+
 
 objectSales = TicketSales(root)
 root.mainloop()
